5c7504ca6d6f52060ded7faf751194478b972a11
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2023-02-04 16:59:40 +0000

    register

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
-export default function app() {
-  return <div>App</div>
+export default function App() {
+  return  (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+
+    </Routes>
+  </BrowserRouter>
+  );
 }
---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+function Register() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("You have submitted the form");
+    }
+    const handleChange = (event) => {}
+  return(
+    <>
+        <FormContainer >
+            <form onSubmit={(event)=>handleSubmit(event)}>
+                <div className="brand">
+                    <img src="" alt=""/>
+                    <h1>PeerAcademy</h1>
+                </div>
+                <input 
+                    type="text" 
+                    placeholder="Username" 
+                    name="username" 
+                    onChange={e=>handleChange(e)}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Student Number" 
+                    name="student number" 
+                    onChange={e=>handleChange(e)}
+                />
+                <input 
+                    type="email" 
+                    placeholder="TCD Email"
+                    name="email" 
+                    onChange={e=>handleChange(e)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password" 
+                    onChange={e=>handleChange(e)}
+                />
+                <input 
+                    type="password"
+                    placeholder="Confirm Password" 
+                    name="confirm password" 
+                    onChange={e=>handleChange(e)}
+                />
+                <button type="submit">Register</button>
+                <span>
+                    Have an Account? Login here <Link to="/login">Login</Link>
+                </span>
+            </form>
+        </FormContainer>
+    </>
+  );
+}
+
+const FormContainer = styled.div``;
+
+export default Register;
