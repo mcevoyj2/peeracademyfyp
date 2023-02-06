93cf45f3262b7529ab04bbe3e22acab055b33a3a
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2023-02-06 18:01:34 +0000

    avatar not working

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,18 +1,61 @@
-import React,{ useEffect} from "react";
+import React,{ useEffect, useState} from "react";
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ToastContainer,toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import loader from "./loader.gif";
 import axios from "axios";
 import { setAvatarRoute } from "../utils/APIRoutes";
+import { Buffer } from "buffer";
 
 export default function SetAvatar() {
-  const api = 'https://api.multiavatar.com/45678945';
+  const api = `https://api.multiavatar.com/45678945`;
   const navigate = useNavigate();
+  const [avatars,setAvatars] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+  const [selectedAvatar,setSelectedAvatar] = useState(undefined);
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
+  useEffect(async () => {
+    const data = [];
+    for (let i = 0; i < 4; i++) {
+      const image = await axios.get(
+        `${api}/${Math.round(Math.random() * 1000)}`
+      );
+      const buffer = new Buffer(image.data);
+      data.push(buffer.toString("base64"));
+    }
+    setAvatars(data);
+    setIsLoading(false);
+  }, []);
   return(
   <>
-  <Container>SetAvatar</Container>;
+  <Container>
+    <div classname="title-container">
+      <h1>Select an Avatar!</h1>
+    </div>
+    <div className="avatars">
+      {
+        avatars.map((avatar,index) => {
+          return (
+            <div 
+            key={index} 
+            className={`avatar ${selectedAvatar === index ? "selected" : "" 
+          }`}>
+            <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" />
+            onClick={() => setSelectedAvatar(index)}
+          </div>
+              )
+        })
+      }
+    </div>
+    </Container>;
   <ToastContainer />
   </>
   );
